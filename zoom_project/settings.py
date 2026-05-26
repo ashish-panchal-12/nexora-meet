11916fb030b8e067c45de6801afa60c7908d08d3
Ashish <panchalab12@gmail.com>
2026-05-27 00:18:25 +0530

    fix render allowed hosts

--- zoom_project/settings.py
+++ zoom_project/settings.py
@@ -11,10 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv(
-    "ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
-).split(",")
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -114,3 +111,8 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com",
+]
