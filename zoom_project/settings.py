f7066f3d90f0ea62df80b13764573e48de928904
Ashish <panchalab12@gmail.com>
2026-06-04 23:48:26 +0530

    debug railway

--- zoom_project/settings.py
+++ zoom_project/settings.py
@@ -9,12 +9,9 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = os.getenv(
-    "ALLOWED_HOSTS",
-    "localhost,127.0.0.1"
-).split(",")
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
