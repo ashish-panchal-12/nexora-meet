a462fe9b97f51be2e04aa7af5999d05a0915125e
Ashish <panchalab12@gmail.com>
2026-06-21 17:23:10 +0530

    Configure Brevo SMTP

--- zoom_project/settings.py
+++ zoom_project/settings.py
@@ -123,9 +123,9 @@ CSRF_TRUSTED_ORIGINS = [
     "https://nexora-meet-production.up.railway.app",
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = "smtp-relay.brevo.com"
+EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
@@ -134,3 +134,5 @@ EMAIL_HOST_USER = os.getenv("BREVO_USER")
 EMAIL_HOST_PASSWORD = os.getenv("BREVO_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+EMAIL_TIMEOUT = 20
